17a9d2b28a2433e340392a362bc2e66068eb16da
Oluwadara Adedeji <adedejioluwadara7@gmail.com>
2024-02-13 09:03:14 +0000

    Edited the DynamoDB code

--- DynamoDB.py
+++ DynamoDB.py
@@ -12,8 +12,8 @@ def table_function():
     # Get the service resource.
     
     session = boto3.Session(
-        aws_access_key_id='AKIAVFPETSJYIUA66ASG',
-        aws_secret_access_key='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'
+        aws_access_key_id='*********',
+        aws_secret_access_key='**********'
     )
     
     dynamodb = session.resource('dynamodb', config=my_config)
